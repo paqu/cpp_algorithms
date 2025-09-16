fe39d67110cf9a4d22878bff767e3d88179ff4f2
Pawel Kusz <pawel.kusz1993@gmail.com>
2025-09-16 19:18:31 +0200

    Add template 'implementation' for quick sort

--- include/algorithms/sorting/quick_sort.hpp
+++ include/algorithms/sorting/quick_sort.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <functional>
 #include <vector>
 
@@ -10,8 +11,40 @@ namespace algorithms::sorting {
  * Not stable
  */
 template <typename T, typename Compare = std::less<T>>
-std::vector<T> quickSort(const std::vector<T> &arr, Compare comp = Compare{});
+void quick_sort_impl(std::vector<T> &arr, int left, int right, Compare comp) {
+    if (left >= right)
+        return;
+
+    // Choose pivot (middle element)
+    T pivot = arr[left + (right - left) / 2];
+    int i = left;
+    int j = right;
+
+    while (i <= j) {
+        while (comp(arr[i], pivot))
+            i++; // arr[i] < pivot
+        while (comp(pivot, arr[j]))
+            j--; // arr[j] > pivot
+
+        if (i <= j) {
+            std::swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+
+    // Recurse on partitions
+    if (left < j)
+        quick_sort_impl(arr, left, j, comp);
+    if (i < right)
+        quick_sort_impl(arr, i, right, comp);
+}
 
 template <typename T, typename Compare = std::less<T>>
-void quickSortInPlace(std::vector<T> &arr, Compare comp = Compare{});
+void quick_sort(std::vector<T> &arr, Compare comp = Compare{}) {
+    if (!arr.empty()) {
+        quick_sort_impl(arr, 0, static_cast<int>(arr.size()) - 1, comp);
+    }
+}
+
 } // namespace algorithms::sorting
