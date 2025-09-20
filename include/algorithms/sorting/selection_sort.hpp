5fab47e659c85f1d83ef7d944d0adce081d3e9cc
Pawel Kusz <pawel.kusz1993@gmail.com>
2025-09-20 11:00:48 +0200

    Implement selection_sort

--- include/algorithms/sorting/selection_sort.hpp
+++ include/algorithms/sorting/selection_sort.hpp
@@ -5,15 +5,42 @@
 namespace algorithms::sorting {
 
 /**
+ * In-place selection sort implementation
+ * Sorts the given vector in-place using selection sort algorithm
+ */
+template <typename T, typename Compare = std::less<T>>
+void selection_sort_impl(std::vector<T> &arr, Compare comp = Compare{}) {
+    if (arr.size() <= 1)
+        return;
+
+    for (size_t i = 0; i < arr.size() - 1; ++i) {
+        // Find the minimum/maximum element in the remaining unsorted array
+        size_t target_idx = i;
+
+        for (size_t j = i + 1; j < arr.size(); ++j) {
+            if (comp(arr[j], arr[target_idx])) {
+                target_idx = j;
+            }
+        }
+
+        // Swap the found minimum/maximum element with the first element
+        if (target_idx != i) {
+            std::swap(arr[i], arr[target_idx]);
+        }
+    }
+}
+
+/**
  * Selection Sort Algorithm
  * Time: O(n²), Space: O(1)
  * Not stable
  */
 template <typename T, typename Compare = std::less<T>>
-std::vector<T> selectionSort(const std::vector<T> &arr,
-                             Compare comp = Compare{});
-
-template <typename T, typename Compare = std::less<T>>
-void selectionSortInPlace(std::vector<T> &arr, Compare comp = Compare{});
+std::vector<T> selection_sort(const std::vector<T> &arr,
+                              Compare comp = Compare{}) {
+    std::vector<T> result = arr; // Copy the input
+    selection_sort_impl(result, comp);
+    return result;
+}
 
 } // namespace algorithms::sorting
